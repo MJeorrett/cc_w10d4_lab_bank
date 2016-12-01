6558df41b7d4e4063b84505d01b8af83cba5e760
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-01 14:18:23 +0000

    adding name to account

--- specs/account_spec.js
+++ specs/account_spec.js
@@ -1,9 +1,16 @@
-var assert = require("assert");
+var assert = require( 'assert' );
+var Account = require( '../account' );
 
-describe("Account", function(){
+describe( "Account", function(){
+
+  var cookiesAccount = null;
+
+  beforeEach( function() {
+    cookiesAccount = new Account( "Cookie" );
+  });
 
   it("should have name passed in constructor", function(){
-    assert.equal("Cookie", cookiesAccount.name);
+    assert.equal( "Cookie", cookiesAccount.name );
   });
 
 });
