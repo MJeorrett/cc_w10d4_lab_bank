4ba70a92042675253efc9d224e4e0ee7fa8a1e89
Clacson <cookiellacson@gmail.com>
2016-12-01 14:10:00 +0000

    first test created

---
+++ specs/account_spec.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("Account", function(){
+
+it("should have name passed in constructor", function(){
+  assert.equal("Cookie", cookiesAccount.name);
+});
+
+});
