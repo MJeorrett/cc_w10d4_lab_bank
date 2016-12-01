89659c2301bad1041d4439ea2fee947c4410b0fe
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-12-01 14:56:45 +0000

    adding findAccount function

--- bank.js
+++ bank.js
@@ -13,6 +13,16 @@ Bank.prototype = {
   addAccount: function(name, balance, type){
     var account = new Account( name, balance, type);
     this.accounts.push(account);
+  },
+  findAccount: function( accountName ) {
+
+    var foundAccount = this.accounts.find( function( account ) {
+
+      var found = account.name === accountName;
+      return found;
+    });
+
+    return foundAccount;
   }
 };
 
