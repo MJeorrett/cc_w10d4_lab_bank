1d1c105d25fc28f56aefe2233a5d169ae2725d2f
Clacson <cookiellacson@gmail.com>
2016-12-01 14:40:19 +0000

    adding test for initial numberOfAccounts

--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -7,9 +7,14 @@ describe( "Bank", function() {
 
   beforeEach(function(){
     myBank = new Bank();
-  })
+  });
 
   it( "should have no accounts at start", function() {
     assert.equal( 0, myBank.accounts.length );
   });
+
+  it("should have numberOfAccounts equal zero at start", function(){
+    assert.equal( 0, myBank.numberOfAccounts() );
+  })
+
 });
